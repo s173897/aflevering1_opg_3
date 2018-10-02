4bbf0734c563f8216423b1f3f83793f1d641cd78
s173897 <40020936+s173897@users.noreply.github.com>
2018-10-02 10:36:53 +0200

    added utilities

--- aflevering1_opg3/randomizer.java
+++ aflevering1_opg3/randomizer.java
@@ -1,11 +1,15 @@
 package aflevering1_opg3;
 
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
 
 public class randomizer {
 	public static void main(String[] args) {
 		Random ran = new Random();
-		int s = ran.nextInt(100 + 1 + 100) - 100;
+		int max=100;
+		int min =-100;
+		int s = ran.nextInt(max + 1 + min) - min;
 		System.out.println(s);
 
 	}
